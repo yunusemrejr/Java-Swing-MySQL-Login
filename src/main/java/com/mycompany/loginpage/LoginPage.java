abe8ea05e2e958771a80217ce7e25701a9e3fdfd
YUNUS EMRE VURGUN <64273622+yunusemrejr@users.noreply.github.com>
2022-06-22 19:08:50 +0300

    Add files via upload

---
+++ src/main/java/com/mycompany/loginpage/LoginPage.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.loginpage;
+
+/**
+ *
+ * @author yunus
+ */
+public class LoginPage {
+
+    public static void main(String[] args) {
+
+
+   Login log = new Login();
+   
+   log.show();
+
+
+    }
+}
